eca08a1a934a1928d92b07e3ff2ce60aa774fc70
paliparsa <121433935+paliparsa@users.noreply.github.com>
2026-09-02 02:33:08 +0300

    Add files via upload

--- public/miniapp/index.php
+++ public/miniapp/index.php
@@ -8,7 +8,7 @@ foreach ($assets as $asset) {
     $path = __DIR__ . '/' . $asset;
     $mtimes[] = file_exists($path) ? filemtime($path) : time();
 }
-$version = 'v3082-crypto-wallet-tracking-' . max($mtimes);
+$version = 'v3083-wallet-scroll-root-' . max($mtimes);
 
 $html = file_get_contents(__DIR__ . '/index.html');
 foreach ($assets as $asset) {
